5cbd57b4556c9cd755d8ea53883adf548bc8d1df
starhappy <konglingxing@126.com>
2018-12-03 15:46:34 +0800

    update manage.py

--- manage.py
+++ manage.py
@@ -3,4 +3,9 @@ from jobplus.app import create_app
 app = create_app('development')
 
 if __name__ == '__main__':
-    app.run()
+    #app.run()
+
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
